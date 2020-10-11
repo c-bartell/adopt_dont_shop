ec2fafe9f53f20a090526e61b7da8a6e78e5c4e5
Curtis Bartell <cbartell980@gmail.com>
2020-10-11 11:23:37 -0600

    Debug shelter_name test

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -14,20 +14,22 @@ RSpec.describe Pet, type: :model do
   end
 
   describe "instance methods:" do
-    shelter = Shelter.create!(
-      name: 'MaxFund Dog Shelter',
-      address: '1005 Galapago Street',
-      city: 'Denver',
-      state: 'CO',
-      zip: '80204-3942'
-    )
-    pet = Pet.create!(
-      image: 'https://cdn.pixabay.com/photo/2016/02/19/11/53/pug-1210025_960_720.jpg',
-      name: 'Admiral Muffins, III',
-      approximate_age: '2',
-      sex: 'M',
-      shelter_id: shelter.id
-    )
-    expect(pet.shelter_name).to eq(shelter.name)
+    it ".shelter_name" do
+      shelter = Shelter.create!(
+        name: 'MaxFund Dog Shelter',
+        address: '1005 Galapago Street',
+        city: 'Denver',
+        state: 'CO',
+        zip: '80204-3942'
+      )
+      pet = Pet.create!(
+        image: 'https://cdn.pixabay.com/photo/2016/02/19/11/53/pug-1210025_960_720.jpg',
+        name: 'Admiral Muffins, III',
+        approximate_age: '2',
+        sex: 'M',
+        shelter_id: shelter.id
+      )
+      expect(pet.shelter_name).to eq(shelter.name)
+    end
   end
 end
