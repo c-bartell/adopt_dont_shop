b9455f3276735b910c6e7f67cd703c46671e817f
Curtis Bartell <cbartell980@gmail.com>
2020-10-11 12:21:28 -0600

    Build out skeleton of test flow

--- spec/features/shelterpets/index_spec.rb
+++ spec/features/shelterpets/index_spec.rb
@@ -9,3 +9,71 @@ require 'rails_helper'
 # - name
 # - approximate age
 # - sex
+
+RSpec.describe "Shelter Pets Index:" do
+  describe "As a visitor" do
+    describe "when I visit '/shelters/:shelter_id/pets'" do
+      describe "then I see each Pet that can be adopted from that Shelter with that shelter_idincluding the pet's:" do
+        before(:all) do
+          @shelter_1 = Shelter.create!(
+            name: 'MaxFund Dog Shelter',
+            address: '1005 Galapago Street',
+            city: 'Denver',
+            state: 'CO',
+            zip: '80204-3942'
+          )
+          @shelter_2 = Shelter.create!(
+            name: 'MaxFund Cat Shelter',
+            address: '720 W. 10th Avenue',
+            city: 'Denver',
+            state: 'CO',
+            zip: '80204'
+          )
+          @shelters = [@shelter_1, @shelter_2]
+          @pet_1 = Pet.create!(
+            image: 'https://cdn.pixabay.com/photo/2016/02/19/11/53/pug-1210025_960_720.jpg',
+            name: 'Admiral Muffins, III',
+            approximate_age: '2',
+            sex: 'M',
+            shelter_id: @shelter_1.id
+          )
+          @pet_2 = Pet.create!(
+            image: 'https://cdn.pixabay.com/photo/2017/01/30/23/19/cat-2022341_960_720.jpg',
+            name: 'Commodore Poptart, Esq.',
+            approximate_age: '6',
+            sex: 'F',
+            shelter_id: @shelter_2.id
+          )
+          @pet_3 = Pet.create!(
+            image: 'https://cdn.pixabay.com/photo/2017/11/15/13/52/bulldog-2952049_960_720.jpg',
+            name: 'Banjo, Dutchess of Snortleshire',
+            approximate_age: '3',
+            sex: 'F',
+            shelter_id: @shelter_1.id
+          )
+          @pets = [@pet_1, @pet_2, @pet_3]
+        end
+        after(:all) do
+          @pets.each do |pet|
+            pet.destroy
+          end
+          @shelters.each do |shelter|
+            shelter.destroy
+          end
+        end
+        it "image" do
+          #code
+        end
+        it "name" do
+          #code
+        end
+        it "approximate age" do
+          #code
+        end
+        it "sex" do
+          #code
+        end
+      end
+    end
+  end
+end
