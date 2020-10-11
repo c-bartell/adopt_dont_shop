001a0097169073cdc6e2ee118af357793bc4832f
Curtis Bartell <cbartell980@gmail.com>
2020-10-11 13:29:18 -0600

    Build out testing skeleton

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -11,3 +11,30 @@ require 'rails_helper'
 # - approximate age
 # - sex
 # - adoptable/pending adoption status
+
+RSpec.describe "Pet Show:" do
+  describe "As a visitor:" do
+    describe "when I visit '/pets/:id'" do
+        describe "then I see the pet with that id including the pet's:" do
+          it "image" do
+            #code
+          end
+          it "name" do
+            #code
+          end
+          it "description" do
+            #code
+          end
+          it "approximate age" do
+            #code
+          end
+          it "sex" do
+            #code
+          end
+          it "adoptable/pending adoption status" do
+            #code
+          end
+        end
+    end
+  end
+end
