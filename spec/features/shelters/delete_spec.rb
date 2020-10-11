aff1fe21132207415ce0cdb620ef3e22b63c5818
Curtis Bartell <cbartell980@gmail.com>
2020-10-10 18:56:43 -0600

    Add test for 'Delete Shelter' link

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -9,3 +9,25 @@ require 'rails_helper'
 # Then a 'DELETE' request is sent to '/shelters/:id',
 # the shelter is deleted,
 # and I am redirected to the shelter index page where I no longer see this shelter
+RSpec.describe "Shelter Delete" do
+  describe "As a visitor" do
+    describe "when I visit a shelter show page" do
+      it "then I see a link to delete the shelter" do
+        shelter = Shelter.create(
+          name: 'MaxFund Dog Shelter',
+          address: '1005 Galapago Street',
+          city: 'Denver',
+          state: 'CO',
+          zip: '80204-3942'
+        )
+        visit("/shelters/#{shelter.id}")
+        expect(page).to have_link('Delete Shelter')
+      end
+    end
+    # describe "when I click the 'Delete Shelter' link" do
+    #   it "then a DELETE request is sent to '/shelters/:id', the shelter is deleted, and I am redirected to the shelter index page where I no longer see this shelter" do
+    #     #expectations
+    #   end
+    # end
+  end
+end
