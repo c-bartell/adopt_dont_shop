9a67b785802d00b845c0d14f7391467eae060eb5
Curtis Bartell <cbartell980@gmail.com>
2020-10-11 12:01:14 -0600

    Create index_spec.rb

---
+++ spec/features/shelterpets/index_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+# User Story 8, Shelter Pets Index
+#
+# As a visitor
+# When I visit '/shelters/:shelter_id/pets'
+# Then I see each Pet that can be adopted from that Shelter with that shelter_id including the Pet's:
+# - image
+# - name
+# - approximate age
+# - sex
