59904b26dd9b56d74bc111b67a1c8755bdf01076
Curtis Bartell <cbartell980@gmail.com>
2020-10-10 23:18:41 -0600

    Add validation and relationship tests

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+RSpec.describe Pet, type: :model do
+  describe "validations" do
+    it { should_validate_presence_of :image }
+    it { should_validate_presence_of :name }
+    it { should_validate_presence_of :approximate_age }
+    it { should_validate_presence_of :sex }
+    it { should_validate_presence_of :shelter_id }
+  end
+
+  describe "relationships" do
+    it { should_belong_to :shelter }
+  end
+end
