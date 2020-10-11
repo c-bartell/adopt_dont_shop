d9ef3fae24c680155cae3ccfad861a85a47aaa67
Curtis Bartell <cbartell980@gmail.com>
2020-10-11 13:23:37 -0600

    Create show_spec.rb

---
+++ spec/features/pets/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+# User Story 9, Pet Show
+#
+# As a visitor
+# When I visit '/pets/:id'
+# Then I see the pet with that id including the pet's:
+# - image
+# - name
+# - description
+# - approximate age
+# - sex
+# - adoptable/pending adoption status
