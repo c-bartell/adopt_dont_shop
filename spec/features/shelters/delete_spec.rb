9e09091bbca3b6ee8b0d0713565596fd354dc655
Curtis Bartell <cbartell980@gmail.com>
2020-10-10 19:13:42 -0600

    Add tests for shelter#destroy action

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -9,7 +9,7 @@ require 'rails_helper'
 # Then a 'DELETE' request is sent to '/shelters/:id',
 # the shelter is deleted,
 # and I am redirected to the shelter index page where I no longer see this shelter
-RSpec.describe "Shelter Delete" do
+RSpec.describe "Shelter Delete:" do
   describe "As a visitor" do
     describe "when I visit a shelter show page" do
       it "then I see a link to delete the shelter" do
@@ -24,10 +24,20 @@ RSpec.describe "Shelter Delete" do
         expect(page).to have_link('Delete Shelter')
       end
     end
-    # describe "when I click the 'Delete Shelter' link" do
-    #   it "then a DELETE request is sent to '/shelters/:id', the shelter is deleted, and I am redirected to the shelter index page where I no longer see this shelter" do
-    #     #expectations
-    #   end
-    # end
+    describe "when I click the 'Delete Shelter' link" do
+      it "then a DELETE request is sent to '/shelters/:id', the shelter is deleted, and I am redirected to the shelter index page where I no longer see this shelter" do
+        shelter = Shelter.create(
+          name: 'MaxFund Dog Shelter',
+          address: '1005 Galapago Street',
+          city: 'Denver',
+          state: 'CO',
+          zip: '80204-3942'
+        )
+        visit("/shelters/#{shelter.id}")
+        click_link('Delete Shelter')
+        expect(current_path).to eq('/shelters')
+        expect(page).to_not have_content('MaxFund Dog Shelter')
+      end
+    end
   end
 end
