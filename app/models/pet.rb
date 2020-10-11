f2ec255fbdd5caabe1674351afd8ab359113f8ac
Curtis Bartell <cbartell980@gmail.com>
2020-10-10 23:30:31 -0600

    Add Pet validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,10 @@
 class Pet < ApplicationRecord
+  # Validations
+  validates_presence_of :image
+  validates_presence_of :name
+  validates_presence_of :approximate_age
+  validates_presence_of :sex
+  validates_presence_of :shelter_id
 
+  # Relationships
 end
